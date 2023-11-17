aa0629aa3b52f34381761bc28603a136f53285f7
kameron <ronaldk1@myumanitoba.ca>
2023-11-16 21:25:48 -0600

    Git-Sync at 2023-11-16 21:25:48 CST

--- src/linux/git_sync.sh
+++ src/linux/git_sync.sh
@@ -51,87 +51,88 @@ else
   exit 1
 fi
 
-# process command line arguments
-if 
-while [ $# -gt 0 ]; do
-  case "$1" in
-    -h|--help)
-      echo "Usage: git-sync [OPTIONS]"
-      echo
-      echo "Options:"
-      echo "  -h, --help     Display this help message"
-      echo "  -u, --update   Update configuration. Usage: -u [CONFIG] [VALUE]"
-      echo "  -a, --add      Add a new watched repo. Usage: -a [REPO-PATH]"
-      echo "  -c, --clear    Remove all log files."
-      exit 0
-      ;;
-    -u|--update)
-      if [[ $# -gt 2 ]]; then
-        update_config "$2" "$3"
-        shift 3
-      else
-        echo "Error: Not enough arguments. Usage: -u [CONFIG] [VALUE]"
+if [ $# -ne 0 ]; then
+  # process command line arguments
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -h|--help)
+        echo "Usage: git-sync [OPTIONS]"
+        echo
+        echo "Options:"
+        echo "  -h, --help     Display this help message"
+        echo "  -u, --update   Update configuration. Usage: -u [CONFIG] [VALUE]"
+        echo "  -a, --add      Add a new watched repo. Usage: -a [REPO-PATH]"
+        echo "  -c, --clear    Remove all log files."
+        exit 0
+        ;;
+      -u|--update)
+        if [[ $# -gt 2 ]]; then
+          update_config "$2" "$3"
+          shift 3
+        else
+          echo "Error: Not enough arguments. Usage: -u [CONFIG] [VALUE]"
+          exit 1
+        fi
+        ;;
+      -a|--add)
+        if [[ $# -gt 1 ]]; then
+          echo "$2" >> "$repo_list_file"
+        fi
+        shift 2
+        ;;
+      -c|--clear)
+        rm "$log_folder/*.log" || echo "Warning: unable to clear .log files"
+        shift 1
+        ;;
+      *)
+        echo "Unknown option: $1"
         exit 1
-      fi
-      ;;
-    -a|--add)
-      if [[ $# -gt 1 ]]; then
-        echo "$2" >> "$repo_list_file"
-      fi
-      shift 2
-      ;;
-    -c|--clear)
-      rm "$log_folder/*.log" || echo "Warning: unable to clear .log files"
-      shift 1
-      ;;
-    *)
-      echo "Unknown option: $1"
-      exit 1
-      ;;
-  esac
-done
-
-# do git sync
-while true; do
-  for repo in "${repo_list[@]}"; do
-    if [ -d "$repo/.git" ]; then
-      # perform: git add -A
-      add_result=$(git -C "$repo" add -A 2>&1)
-      if [ $? -ne 0 ]; then
-        log "ERROR" "$add_result"
-      fi
-      # perform: git commit -m "message"
-      message="Git-Sync at $(date '+%Y-%m-%d %H:%M:%S %Z')"
-      commit_result=$(git -C "$repo" commit -m "$message" 2>&1)
-      if [ $? -ne 0 ]; then
-        if [[ $commit_result != *"nothing to commit"* ]]; then
-          log "ERROR" "$commit_result"
-        fi
-      else
-        log "INFO" "$commit_result"
-      fi
-      # perform: git pull
-      pull_result=$(git -C "$repo" pull 2>&1)
-      if [ $? -ne 0 ]; then
-        log "ERROR" "$pull_result"
-      else
-        if [[ $pull_result != *"up to date"* ]]; then
-          log "INFO" "$pull_result"
-        fi
-      fi
-      # perform: git push
-      push_result=$(git -C "$repo" push 2>&1)
-      if [ $? -ne 0 ]; then
-        log "ERROR" "$push_result"
-      else
-        if [[ $push_result != *"up-to-date"* ]]; then
-          log "INFO" "$push_result"
-        fi
-      fi
-    else
-      log "WARNING" "$repo is not a .git repository"
-      remove_element repo_list "$repo"
-    fi
+        ;;
+    esac
   done
-  sleep "$INTERVAL"
-done
+else
+  # do git sync
+  while true; do
+    for repo in "${repo_list[@]}"; do
+      if [ -d "$repo/.git" ]; then
+        # perform: git add -A
+        add_result=$(git -C "$repo" add -A 2>&1)
+        if [ $? -ne 0 ]; then
+          log "ERROR" "$add_result"
+        fi
+        # perform: git commit -m "message"
+        message="Git-Sync at $(date '+%Y-%m-%d %H:%M:%S %Z')"
+        commit_result=$(git -C "$repo" commit -m "$message" 2>&1)
+        if [ $? -ne 0 ]; then
+          if [[ $commit_result != *"nothing to commit"* ]]; then
+            log "ERROR" "$commit_result"
+          fi
+        else
+          log "INFO" "$commit_result"
+        fi
+        # perform: git pull
+        pull_result=$(git -C "$repo" pull 2>&1)
+        if [ $? -ne 0 ]; then
+          log "ERROR" "$pull_result"
+        else
+          if [[ $pull_result != *"up to date"* ]]; then
+            log "INFO" "$pull_result"
+          fi
+        fi
+        # perform: git push
+        push_result=$(git -C "$repo" push 2>&1)
+        if [ $? -ne 0 ]; then
+          log "ERROR" "$push_result"
+        else
+          if [[ $push_result != *"up-to-date"* ]]; then
+            log "INFO" "$push_result"
+          fi
+        fi
+      else
+        log "WARNING" "$repo is not a .git repository"
+        remove_element repo_list "$repo"
+      fi
+    done
+    sleep "$INTERVAL"
+  done
+fi
